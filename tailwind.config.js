588d7c88d4bf7f442e8379d326d3d2e181535b7a
Anonymous <68388009+anonymousaga@users.noreply.github.com>
2024-06-16 11:53:49 -0400

    rttry

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,29 @@ const colors = require('tailwindcss/colors')
 
 
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                   //just add this below and your all other tailwind colors willwork
-                ...colors
-            }
-        },
-        content: [
-            "./*.{html,js,svelte,ts}",
-            "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-          ],
-        
-          plugins: [
-            require('flowbite/plugin')
-          ],
-        
-          darkMode: 'class',
-    
-},
-experimental: {
-  optimizeUniversalDefaults: true
-}
+  theme: {
+    extend: {
+      colors: {
+        //just add this below and your all other tailwind colors willwork
+        ...colors
+      },
+      transitionProperty: {
+        'spacing': 'margin, padding',
+      },
+    },
+    content: [
+      "./*.{html,js,svelte,ts}",
+      "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    ],
+
+    plugins: [
+      require('flowbite/plugin')
+    ],
+
+    darkMode: 'class',
+
+  },
+  experimental: {
+    optimizeUniversalDefaults: true
+  }
 }
